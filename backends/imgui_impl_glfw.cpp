53ca6d06c486b6daa86548aae11cc927b89719a0
SuperVisor <SuperVisor@SUPERVISOR>
2021-02-23 17:29:32 -0500

    added premake to fork

--- backends/imgui_impl_glfw.cpp
+++ backends/imgui_impl_glfw.cpp
@@ -39,9 +39,11 @@
 
 #include "imgui.h"
 #include "imgui_impl_glfw.h"
+//#include "SVEpch.h"
 
 // GLFW
 #include <GLFW/glfw3.h>
+//#include <glad/glad.h>
 #ifdef _WIN32
 #undef APIENTRY
 #define GLFW_EXPOSE_NATIVE_WIN32
--- backends/imgui_impl_opengl3.cpp
+++ backends/imgui_impl_opengl3.cpp
@@ -76,9 +76,10 @@
 #if defined(_MSC_VER) && !defined(_CRT_SECURE_NO_WARNINGS)
 #define _CRT_SECURE_NO_WARNINGS
 #endif
-
+//#include "SVEpch.h"
 #include "imgui.h"
 #include "imgui_impl_opengl3.h"
+//#include "glad/glad.h"
 #include <stdio.h>
 #if defined(_MSC_VER) && _MSC_VER <= 1500 // MSVC 2008 or earlier
 #include <stddef.h>     // intptr_t
